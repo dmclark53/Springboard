4fd010fdd18659c330f485ee5892dffbb06b50d4
dmclark53 <dmclark5@gmail.com>
2021-02-22 18:32:40 -0800

    Created plot_kdes() function.

--- Capstone-Project-Two/src/viz/viz.py
+++ Capstone-Project-Two/src/viz/viz.py
@@ -22,3 +22,21 @@ def plot_ecdfs(df, columns_list, num_cols, exclude_zeros=False):
             sns.ecdfplot(x=column, data=df, ax=ax[row_i, col_i])
     plt.tight_layout()
     plt.show()
+
+
+def plot_kdes(df, columns_list, num_cols, exclude_zeros=False):
+
+    num_plots = len(columns_list)
+    num_rows = num_plots // num_cols
+
+    fix, ax = plt.subplots(num_rows, num_cols, figsize=(12, 8))
+    for i, column in enumerate(columns_list):
+        row_i = i // num_cols
+        col_i = i % num_cols
+        if exclude_zeros:
+            non_zero = df[column] != 0.0
+            sns.kdeplot(x=column, hue='status_group', data=df[non_zero], ax=ax[row_i, col_i])
+        else:
+            sns.kdeplot(x=column, hue='status_group', data=df, ax=ax[row_i, col_i])
+    plt.tight_layout()
+    plt.show()
