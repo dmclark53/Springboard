2c53c8202228fffe2d94bfad0ef3c8feed93b2a9
dmclark53 <dmclark5@gmail.com>
2021-02-24 12:58:00 -0800

    Updated numerical variable analysis, removing many zero values.

--- Capstone-Project-Two/src/viz/viz.py
+++ Capstone-Project-Two/src/viz/viz.py
@@ -11,15 +11,19 @@ def plot_ecdfs(df, columns_list, num_cols, exclude_zeros=False):
     num_plots = len(columns_list)
     num_rows = num_plots // num_cols
 
-    fix, ax = plt.subplots(num_rows, num_cols, figsize=(12, 8))
+    fix, ax = plt.subplots(num_rows, num_cols, figsize=(12, 4*num_rows))
     for i, column in enumerate(columns_list):
         row_i = i // num_cols
         col_i = i % num_cols
+        if len(ax.shape) == 2:
+            next_ax = ax[row_i, col_i]
+        else:
+            next_ax = ax[i]
         if exclude_zeros:
             non_zero = df[column] != 0.0
-            sns.ecdfplot(x=column, data=df[non_zero], ax=ax[row_i, col_i])
+            sns.ecdfplot(x=column, data=df[non_zero], ax=next_ax)
         else:
-            sns.ecdfplot(x=column, data=df, ax=ax[row_i, col_i])
+            sns.ecdfplot(x=column, data=df, ax=next_ax)
     plt.tight_layout()
     plt.show()
 
@@ -29,15 +33,19 @@ def plot_kdes(df, columns_list, num_cols, exclude_zeros=False):
     num_plots = len(columns_list)
     num_rows = num_plots // num_cols
 
-    fix, ax = plt.subplots(num_rows, num_cols, figsize=(12, 8))
+    fix, ax = plt.subplots(num_rows, num_cols, figsize=(16, 4*num_rows))
     for i, column in enumerate(columns_list):
         row_i = i // num_cols
         col_i = i % num_cols
+        if len(ax.shape) == 2:
+            next_ax = ax[row_i, col_i]
+        else:
+            next_ax = ax[i]
         if exclude_zeros:
             non_zero = df[column] != 0.0
-            sns.kdeplot(x=column, hue='status_group', data=df[non_zero], ax=ax[row_i, col_i])
+            sns.kdeplot(x=column, hue='status_group', data=df[non_zero], ax=next_ax)
         else:
-            sns.kdeplot(x=column, hue='status_group', data=df, ax=ax[row_i, col_i])
+            sns.kdeplot(x=column, hue='status_group', data=df, ax=next_ax)
     plt.tight_layout()
     plt.show()
 
