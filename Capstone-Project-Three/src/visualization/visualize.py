3cac615a59a6a4e645963ade13d732b8eda02319
dmclark53 <dmclark5@gmail.com>
2021-06-28 17:10:09 -0700

    Compared images by color channel.

--- Capstone-Project-Three/src/visualization/visualize.py
+++ Capstone-Project-Three/src/visualization/visualize.py
@@ -35,6 +35,33 @@ def display_images(image_list, num_cols):
         img = cv.imread(os.path.join(con.RAW_IMAGES_DIR, image_file))
         fig.add_subplot(num_rows, num_cols, i+1)
         plt.title(image_file.split('/')[0])
+        plt.axis('off')
         plt.imshow(img)
+    plt.tight_layout()
+    plt.show()
+
+
+def display_images_by_channel(image_list):
+    """
+    Display images by channel.
+
+    :param image_list: List of image file names.
+    :type image_list: list
+    """
+
+    num_rows = len(image_list)
+    img_tmp = cv.imread(os.path.join(con.RAW_IMAGES_DIR, image_list[0]))
+    num_cols = img_tmp.shape[2]
+    fig = plt.figure(figsize=(12, 48))
+    counter = 1
+    colors = ['Reds', 'Blues', 'Greens']
+    for i, image_file in enumerate(image_list):
+        img = cv.imread(os.path.join(con.RAW_IMAGES_DIR, image_file))
+        for channel in range(num_cols):
+            fig.add_subplot(num_rows, num_cols, counter)
+            plt.title(f"{image_file.split('/')[0]} ({colors[channel][:-1].lower()})")
+            plt.axis('off')
+            plt.imshow(img[:, :, channel], cmap=colors[channel])
+            counter += 1
     plt.tight_layout()
     plt.show()
