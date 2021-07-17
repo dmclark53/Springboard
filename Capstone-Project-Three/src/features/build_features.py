0f886f6398533f6fb08001645324597445c3940c
dmclark53 <dmclark5@gmail.com>
2021-07-17 10:04:35 -0700

    Renamed flatten_images() function and refactored to make more general.

--- Capstone-Project-Three/src/features/build_features.py
+++ Capstone-Project-Three/src/features/build_features.py
@@ -10,24 +10,31 @@ import numpy as np
 from src import constants as con
 
 
-def flatten_images(image_file_list, rescale=False):
-    flattened_images = []
+def preprocess_images(image_file_list, flatten=False, rescale=False):
+    preprocessed_images = []
     start = time()
-    for image_file in image_file_list:
+    print('Preprocessing Images:')
+    if flatten:
+        print(' * Flattening.')
+    if rescale:
+        print(' * Rescaling.')
+    for i, image_file in enumerate(image_file_list):
         image = cv.imread(os.path.join(con.RAW_IMAGES_DIR, image_file))
         if rescale:
             scale = 0.5
             new_size = (int(image.shape[1]*scale), int(image.shape[0]*scale))
             image = cv.resize(image, new_size)
-        flattened_images.append(image.flatten())
+        if flatten:
+            image = image.flatten()
+        preprocessed_images.append(image.flatten())
     end = time()
     elapsed = end - start
     time_unit = 'seconds'
     if elapsed > 60:
         elapsed = elapsed / 60
         time_unit = 'minutes'
-    print(f'It took {elapsed:0.3f} {time_unit} to load and flatten {len(image_file_list)} images.')
-    flattened_images_array = np.array(flattened_images)
+    print(f'It took {elapsed:0.3f} {time_unit} to load and preprocess {len(image_file_list)} images.')
+    flattened_images_array = np.array(preprocessed_images)
     memory_size = flattened_images_array.size * flattened_images_array.itemsize
     memory_unit = 'bytes'
     if memory_size >= 1e9:
@@ -41,5 +48,5 @@ def flatten_images(image_file_list, rescale=False):
         memory_unit = 'Kb'
     else:
         pass
-    print(f'The array of flattened images takes up {memory_size:0.3f} {memory_unit} of memory.')
+    print(f'The array of preprocessed images takes up {memory_size:0.3f} {memory_unit} of memory.')
     return flattened_images_array
