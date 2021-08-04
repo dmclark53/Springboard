4367b6c2394ff4cccfa421f556223374c6f25b09
dmclark53 <dmclark5@gmail.com>
2021-08-04 16:10:37 -0700

    Updated load_train_test() function to accept training data directory.

--- Capstone-Project-Three/src/data_setup/make_dataset.py
+++ Capstone-Project-Three/src/data_setup/make_dataset.py
@@ -5,13 +5,18 @@ import pickle
 from src import constants as con
 
 
-def load_train_test():
+def load_train_test(data_dir):
     """
     Load pickled training and test data from file.
     """
 
-    file_list = ['X_train', 'X_test', 'y_train', 'y_test']
-    data_sets = []
-    for filename in file_list:
-        data_sets.append(pickle.load(open(os.path.join(con.PROCESSED_DATA_DIR, filename), 'rb')))
-    return tuple(data_sets)
+    data_directories = [os.path.split(x[0])[-1] for x in os.walk(con.PROCESSED_DATA_DIR)]
+    if data_dir in data_directories:
+        file_list = ['X_train', 'X_test', 'y_train', 'y_test']
+        data_sets = []
+        for filename in file_list:
+            data_sets.append(pickle.load(open(os.path.join(con.PROCESSED_DATA_DIR, data_dir, filename), 'rb')))
+        return tuple(data_sets)
+    else:
+        print(f'The data directory {data_dir} not in {con.PROCESSED_DATA_DIR}.')
+        return None, None, None, None
